2b5d84eb18ff2c9e2a2f48765c2f247575090b67
yahyaraees <yahyaraees667@gmail.com>
2025-05-02 00:15:27 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,268 @@
+# Qno 1    Create two integer variables length and breadth and assign values then check
+#  if they are square values or rectangle values.
+# ie: if both values are equal then it's square otherwise rectangle.
+
+
+# length = 10
+# breath = 14
+
+# if length == breath:
+#   print("squre value")
+# else:
+#   print("Ragtangular")
+
+# /////////////////
+
+# Q.2: Take two variables and store age then using 
+# if/else condition to determine oldest and youngest among them.
+
+
+
+# abc = 16
+# old = 16
+
+# if  abc < old:
+#   print("abc is short num")
+# elif old > abc:
+#   print("old num is longer")
+# else:
+#   print("both are same") 
+
+
+
+# age1 = 2
+# age2 = 80
+
+
+# if age1 > age2:
+#  print("age1 is older")
+#  print("age2 is young")
+
+# elif age2 > age1:
+#   print("age2 is older ") 
+#   print("age1 is young  ") 
+
+# else:
+#    print("both are of the age same") 
+
+
+# ///////////////////
+
+# Q.3: A student will not be allowed to sit in exam if his/her attendance is less than 75%. Create integer variables and assign value:
+# Number of classes held = 16,
+# Number of classes attended = 10,
+# and print percentage of class attended.
+# Is student is allowed to sit in exam or not?
+
+
+# class_held = 11
+# clas_attend = 10 
+
+# percentage  = clas_attend/ class_held * 100
+
+# if percentage >=75:
+#   print(f"you are allowed {percentage:.2f}%")
+# else:
+#   print(f"you are not allowed {percentage:.2}%")    
+
+
+
+# Q.4: Create integer variable assign any year to it and check if a year is leap year or not.
+# If a year is divisible by 4 then it is leap year but if the year is century year like 2000, 1900, 2100 then it must be divisible by 400.
+# i.e: Use % ( modulus ) operator. 
+
+
+# leap_year = int(2004)
+# leap_century = int(1900)
+
+# if leap_year % 4 == 0:
+#   print("leap year")
+
+# if leap_century % 100 == 0:
+#   if leap_century % 400 == 0:
+#     print("leap century year")
+#   else:
+#    print("century year but not a leap year")
+# else:
+#    print("is not leap_year and leap_century")   
+
+
+# /////////////////
+
+# Q.5  Write a program to read temperature in centigrade and display a
+#  suitable message according to temperature:
+
+
+# temperature = float(input("Enter your number"))
+
+# if temperature <0:
+#     print("Freezing temperature")
+# elif temperature >=0 and temperature <=10:
+#     print("Very cold temperature")
+# elif temperature >10 and temperature <=20:
+#     print("Cold temperature")   
+# elif temperature >20 and temperature <=30:
+#     print("Normal temperature")    
+# elif temperature >30 and temperature <=40:
+#     print("Its Hot")    
+# elif temperature >40 :
+#     print("Its very Hot")
+# else:
+#     print("Invalid input. please Enter a valid input")
+
+
+# temperature = float(input("Enter the temperature in °C: "))
+
+# if temperature < 0:
+#     print("Freezing weather")
+# elif temperature <= 10:
+#     print("Very Cold weather")
+# elif temperature <= 20:
+#     print("Cold weather")
+# elif temperature <= 30:
+#     print("Normal in Temp")
+# elif temperature <= 40:
+#     print("It's Hot")
+# else:
+#     print("It's Very Hot")
+
+
+# ./////////////////
+# Q.6: Write a program to check whether an alphabet is a vowel or consonant.
+
+
+# user = str(input("Enter vowel words "))
+
+# if not user.isalpha():
+#      print("Invalid user input")  
+ 
+# elif  user.lower() == "a" or user == "e" or  user == "i" or  user == "o" or  user == "u":
+#      print("it's a vowel words")  
+# else:
+#     print("it's a consonant")    
+
+
+
+
+
+
+# //////////////////////
+
+# Q.7: Write a program to calculate and 
+# print the Electricity bill of a given customer. 
+# Create variable for customer id, name, unit consumed by the user, 
+# bill_amount and print the total amount the customer needs to pay. The charge are as follow :
+
+
+
+# id = int(input("Enter your ID "))
+# name = str(input("Enter your Name "))
+# unit = float(input("Enter your Unit "))
+# rate = 0
+# if unit <=199:
+#   rate = 1.20
+#   print("\n Your unit fall under Rs. 1.20 per unit")
+# elif unit >= 200 and  unit < 400:
+#   rate = 1.50 
+#   print("\ny Your unit fall under Rs. 1.50 per unit")
+# elif unit >=400 and unit < 600:
+#   rate = 1.80
+#   print(" \n Your unit fall under Rs. 1.80 per unit ---")
+# elif unit >= 600:
+#   rate = 2.00
+#   print("\n Your unit fall under Rs. 2.00 per unit")
+# else: 
+#   print("\n Invid input range")
+
+# bill_amount = unit * rate
+ 
+# print("\n--- Electricity bill ---") 
+# print(f"Customer IDNO  {id}")
+# print(f"Customer Name  {name}")
+# print(f"Unit Consumed {unit}")
+# print(f"Amount Charges @Rs. {rate} per unit : {bill_amount:.2f}")
+# print("Net Bill Amount :", round(bill_amount,2), "Rupees — You are a VIP customer!")
+
+
+    
+
+# Q8: Create a marksheet using operators of at least 5 subjects and 
+# output should have Student Name, Student Roll Number, Class, Percentage, Grade Obtained etc.
+# i.e: Percentage should be rounded upto 2 decimal places only.
+
+
+
+# student_Name = str("hassan")
+# Roll_Num = int(1300)
+# class_1 = int(1)
+
+# english = 80
+# urdu = 80
+# math = 33
+# islamiat = 88
+# science = 90
+
+# total_marks = english + urdu + math + islamiat + science
+# percentage = round((total_marks / 500 )* 100,2)
+
+# if(percentage >= 80 and percentage <= 100 ):
+#      grade = "A+"
+ 
+# elif(percentage >=70  and percentage < 80 ):
+#      grade = "A"
+# elif(percentage >= 60 and percentage < 70 ):
+#      grade  =  "B"
+ 
+ 
+# elif(percentage >= 50 and percentage < 60):
+#     grade = "C"
+
+# elif(percentage >= 33 and percentage < 50 ):
+#    grade = "D"
+ 
+# elif(percentage < 33):
+#    grade = "fail"
+ 
+# else:
+#    grade = "Invalid"
+
+
+# print("\n ---- MarkSheet ---- ")
+# print("Student Name :",student_Name)  
+# print("Roll Number  :",Roll_Num)
+# print("Class        :",class_1) 
+# print("Total Marks  :",total_marks, " / 500")
+# print("percentage   :",percentage,"%")
+# print("grade        :",grade)
+# print("--------------------")
+
+
+# Q9: Check if the number is even or odd, If number is 
+# even then check if this is divisible by 5 or not & if number is odd then check
+# if this is divisible by 7 or not.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
