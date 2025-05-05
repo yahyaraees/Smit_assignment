650a5d67f0010a5b8b6140caff305fb1e5495863
yahyaraees <yahyaraees667@gmail.com>
2025-05-06 00:15:09 +0500

    update

--- app2.py
+++ app2.py
@@ -1,2 +1,18 @@
-employee_name = ["yahyaraees","Abdullah","Yasir","Affan"]
-print(employee_name)
+# Q.1: Create a list of names and print all names using list.
+
+# employee_name = ["yahyaraees","Abdullah","Yasir","Affan"]
+# print(employee_name)
+
+
+# Q.2: Create an empty list of type string called days.
+# Use the add method to add names of 7 days and print all days.
+
+names = []
+names.append("Monday")
+names.append("Tuesday")
+names.append("Wednesday")
+names.append("Thursday")
+names.append("Friday")
+names.append("Saturday")
+names.append("Sunday")
+print(names)
