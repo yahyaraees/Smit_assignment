c057d61f854bb5b0cdebb3be6c826fbe7c608214
yahyaraees <yahyaraees667@gmail.com>
2025-06-22 23:18:49 +0500

    update

--- practice2.py
+++ practice2.py
@@ -1,10 +1,42 @@
-for i in range(1,51):
+# 1st  task ////
+
+# for i in range(1,51):
     
-  if i % 3 == 0 and i % 5 == 0:
-    print(f'FizzBuzz',i)
-  elif i % 3 == 0:
-    print(f'fizz',i) 
-  elif i % 5 == 0:
-    print(f'buzz',i)
-  else:
-    print(i)           
+#   if i % 3 == 0 and i % 5 == 0:
+#     print(f'FizzBuzz',i)
+#   elif i % 3 == 0:
+#     print(f'fizz',i) 
+#   elif i % 5 == 0:
+#     print(f'buzz',i)
+#   else:
+#     print(i)     
+
+# 2nd task       /////////
+
+# palindrome = 'madam'
+
+# istrue = True
+
+# while istrue:
+#   input_palindrome = input('Enter your name ')
+
+#   if palindrome == input_palindrome:
+#     print('palindrome')
+#   else:
+#     print('not palindrome')  
+
+
+istrue = True
+
+while istrue:
+    input_palindrome = input("Enter a word (or type 'exit' to quit): ").strip()
+
+    if input_palindrome.lower() == "exit":
+        break
+
+    if input_palindrome == input_palindrome[::-1]:
+        print("Palindrome")
+    else:
+        print("Not Palindrome")
+
+
